24bc80ec0166efa150234f800eed828229eb02c5
jourdainraphael <Jourdain.raphael@hotmail.fr>
2023-06-12 16:53:55 +0200

    personnalisation des 3 pages du them enfant

--- functions.php
+++ functions.php
@@ -7,6 +7,7 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array(), '1.0.0', 'all' );
 }
 
+
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 
